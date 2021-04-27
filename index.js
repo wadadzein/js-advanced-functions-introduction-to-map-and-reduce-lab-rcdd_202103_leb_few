800745864c42374a50a03b3ffd8ff096e0306375
wadad zein <wadad_zein@hotmail.com>
2021-04-27 20:10:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+// function map(sourceArray, func){
+//   return sourceArray.map(func);
+// }
+// const sourceArray= [1,2,3,4];
+// const mapToNegativize=
+//   sourceArray.map(function(item){
+//     return item * -1;
+//   });
+// console.log (mapToNegative);
+
+function mapToNegativize(sourceArray){
+  return sourceArray.map(function(item){
+    return item*-1;
+  });
+}
+function mapToNoChange(sourceArray){
+ return sourceArray.map(function(item){
+    return item;
+  });
+}
+
+
+function mapToDouble(sourceArray){
+return sourceArray.map(function(item){
+    return item * 2;
+  });
+}
+function mapToSquare(sourceArray){
+return sourceArray.map(function(item){
+    return item * item;
+  });
+}
+function reduceToTotal(sourceArray, startingPoint=0){
+  return sourceArray.reduce(function(result,item){
+    return result+item;
+  }, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray){
+  return sourceArray.reduce(function(item, result){
+    if (!!item==true && !!result==true){
+     return true; 
+    }
+    else{
+      return false;
+    }
+  });
+}
+
+function reduceToAnyTrue(sourceArray){
+  return sourceArray.reduce(function(item,result){
+    if (result===true){
+      return true;
+    }else{
+      return false;
+    }
+  });
+}

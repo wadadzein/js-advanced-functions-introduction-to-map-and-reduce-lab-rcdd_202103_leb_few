ff6babe966d830adda47d5ac33ae8a5d64352d5b
wadad zein <wadad_zein@hotmail.com>
2021-04-27 19:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// function map(sourceArray, func){
+//   return sourceArray.map(func);
+// }
+// const sourceArray= [1,2,3,4];
+// const mapToNegativize=
+//   sourceArray.map(function(item){
+//     return item * -1;
+//   });
+// console.log (mapToNegative);
+
+function mapToNegativize(sourceArray){
+  sourceArray.map(function(item){
+    return item*-1;
+  });
+}
+function mapToNoChange(sourceArray){
+ sourceArray.map(function(item){
+    return item;
+  });
+}
+
+
+function mapToDouble(sourceArray){
+sourceArray.map(function(item){
+    return item * 2;
+  });
+}
+function mapToSquare(sourceArray){
+sourceArray.map(function(item){
+    return item * item;
+  });
+}
